638db9dfc96fa6321752f911c32c78f4d7ca0183
Htet Linn Aung <165995026+unus-corvus@users.noreply.github.com>
2024-04-05 16:55:14 +0630

    Add files via upload

---
+++ atm_algorithm.py
@@ -0,0 +1,37 @@
+# Algorithm for withdrawing a sum of money at an ATM
+
+# Step 1: Insert Your ATM Card
+print("Locate the nearest ATM")
+print("Find the card slot on the machine")
+print("Insert your ATM card into the card slot")
+
+# Step 2: Enter Your Pin
+print("Carefully enter your PIN")
+print("Make sure to shield the keypad from others")
+
+# Step 3: Select Withdraw Option
+print("The ATM will display a menu of options")
+print("Select the Withdraw option by touching the screen or pressing the corresponding button")
+
+# Step 4: Enter Withdraw Amount
+print("Type the desired amount")
+print("Make sure you have enough money")
+
+# Step 5: Confirm Withdraw
+print("Check the displayed amount")
+print("If the amount is correct, confirm the withdraw by pressing the Confirm or Enter button")
+
+# Step 6: Wait for processing
+print("Wait patiently")
+
+# Step 7: Collect Cash
+print("Collect your money from the cash dispenser")
+print("Count the notes")
+
+#Step 8: Take Your Card And Receipt
+print("Retrieve your ATM card from the machine")
+print("Choose whether you want a receipt or not")
+
+#Step 9: End Transaction
+print("Ensure that you have all your belongings")
+print("Leave the ATM machine")
